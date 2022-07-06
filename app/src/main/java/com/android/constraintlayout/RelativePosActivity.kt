deafa8ae1b1e388dc52397050dccf13f0a83a70f
NishonboevIslombek <70129973+NishonboevIslombek@users.noreply.github.com>
2022-07-06 15:18:16 +0500

    Updates in Packages and also Activities

--- app/src/main/java/com/android/constraintlayout/RelativePosActivity.kt
+++ app/src/main/java/com/android/constraintlayout/RelativePosActivity.kt
@@ -2,8 +2,9 @@ package com.android.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.constraintlayout.R
 
-class MainActivity : AppCompatActivity() {
+class RelativePosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
