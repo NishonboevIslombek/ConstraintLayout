cb81325f5d54d6147792a0f9e9a83af2c80ae0d0
NishonboevIslombek <70129973+NishonboevIslombek@users.noreply.github.com>
2022-07-06 15:17:10 +0500

    Using Margins(with GONE View too) in ConstraintLayout

---
+++ app/src/main/java/com/android/constraintlayout/MarginsActivity.kt
@@ -0,0 +1,12 @@
+package com.android.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.constraintlayout.R
+
+class MarginsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_margins)
+    }
+}
