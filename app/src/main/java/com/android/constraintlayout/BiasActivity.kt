5c1444c2eac304a53a02aa2b813835691161544e
NishonboevIslombek <70129973+NishonboevIslombek@users.noreply.github.com>
2022-07-06 15:15:59 +0500

    Using Bias in ConstraintLayout

---
+++ app/src/main/java/com/android/constraintlayout/BiasActivity.kt
@@ -0,0 +1,11 @@
+package com.android.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BiasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bias)
+    }
+}
