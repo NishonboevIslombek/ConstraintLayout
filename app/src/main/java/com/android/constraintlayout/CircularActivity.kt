2738fbfbd884adc2fdb277028b39994a8c5f27da
NishonboevIslombek <70129973+NishonboevIslombek@users.noreply.github.com>
2022-07-06 15:34:17 +0500

    Circular positioning in ConstraintLayout

---
+++ app/src/main/java/com/android/constraintlayout/CircularActivity.kt
@@ -0,0 +1,11 @@
+package com.android.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CircularActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_circular)
+    }
+}
