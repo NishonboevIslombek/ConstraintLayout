5e3e869a496cdef39283b4e0323d77479c78929c
NishonboevIslombek <70129973+NishonboevIslombek@users.noreply.github.com>
2022-07-07 17:25:08 +0500

    Dimensions in ConstraintLayout

---
+++ app/src/main/java/com/android/constraintlayout/DimensionsActivity.kt
@@ -0,0 +1,11 @@
+package com.android.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DimensionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dimensions)
+    }
+}
